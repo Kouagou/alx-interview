e3eba28bbd9048129e617b4e93d5c5a07406bd69
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-11-04 19:19:48 +0100

    Task 0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -22,15 +22,42 @@ def validUTF8(data):
         elif data[i] & 0b11111000 == 0b11110000:
             # 4-byte utf-8 character encoding
             span = 4
-            check_next_body(data, i, span)
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
         elif data[i] & 0b11110000 == 0b11100000:
             # 3-byte utf-8 character encoding
             span = 3
-            check_next_body(data, i, span)
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
         elif data[i] & 0b11100000 == 0b11000000:
             # 2-byte utf-8 character encoding
             span = 2
-            check_next_body(data, i, span)
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
         else:
             return False
     return True
