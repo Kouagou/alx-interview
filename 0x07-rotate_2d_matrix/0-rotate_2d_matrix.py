754433b2786d9eaf915259f292df344ba4a76516
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-11-25 00:16:07 +0100

    Task 0. Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+2D matrix rotation module.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+        Rotates an n by n 2D matrix in place.
+        Args:
+            matrix (list): n by n 2D matrix.
+        Returns:
+            None.
+    """
+    if type(matrix) is not list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) is list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
